c77bf28ed4cf287068d6bd4fbd264316f1697908
zhutf123 <tengfei.zhu@qunar.com>
2022-05-17 10:55:25 +0800

    update upload image

--- src/main/java/com/demai/cornel/util/AliStaticSourceUtil.java
+++ src/main/java/com/demai/cornel/util/AliStaticSourceUtil.java
@@ -6,6 +6,7 @@ package com.demai.cornel.util;
 import com.aliyun.oss.OSS;
 import com.aliyun.oss.OSSClientBuilder;
 import com.aliyun.oss.OSSException;
+import com.aliyun.oss.model.PutObjectRequest;
 import com.aliyuncs.DefaultAcsClient;
 import com.aliyuncs.auth.sts.AssumeRoleRequest;
 import com.aliyuncs.auth.sts.AssumeRoleResponse;
@@ -23,6 +24,7 @@ import com.aliyuncs.vod.model.v20170321.GetPlayInfoRequest;
 import com.aliyuncs.vod.model.v20170321.GetPlayInfoResponse;
 import lombok.extern.slf4j.Slf4j;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStream;
 
@@ -60,10 +62,10 @@ import java.io.InputStream;
             "{\n" + "  \"Version\": \"1\",\n" + "  \"Statement\": [\n" + "    {\n" + "      \"Action\": \"vod:*\",\n"
                     + "      \"Resource\": \"*\",\n" + "      \"Effect\": \"Allow\"\n" + "    }\n" + "  ]\n" + "}";
 
-    public static void main(String[] args) {
-        log.info(getVideoUrl("3e8a569162c646d8bb5f399dfa73177b"));
-        log.info(getImageUrl("5e8236886e4845e3bcf61cb8f3a51f41"));
-    }
+//    public static void main(String[] args) {
+//        log.info(getVideoUrl("3e8a569162c646d8bb5f399dfa73177b"));
+//        log.info(getImageUrl("5e8236886e4845e3bcf61cb8f3a51f41"));
+//    }
 
     public static String uploadVideo(String showName, String path) {
         try {
@@ -236,7 +238,6 @@ import java.io.InputStream;
         // 填写Bucket名称，例如examplebucket。
         String bucketName = "outin-4bfcaac9c80e11ecbfcd00163e021072";
         // 填写Object完整路径，完整路径中不能包含Bucket名称，例如exampledir/exampleobject.txt。
-        //String objectName = "default/exampleobject.txt";
         // 如果未指定本地路径，则默认从示例程序所属项目对应本地路径中上传文件流。
         //String filePath= "D:\\localpath\\examplefile.txt";
         // 创建OSSClient实例。
@@ -252,6 +253,49 @@ import java.io.InputStream;
                             + oe.getErrorMessage() + "Error Code:" + oe.getErrorCode() + "Request ID:" + oe
                             .getRequestId() + "Host ID:" + oe.getHostId());
 
+        } finally {
+            if (ossClient != null) {
+                ossClient.shutdown();
+            }
+        }
+    }
+
+
+
+
+    public static void main(String[] args) throws Exception {
+        // Endpoint以华东1（杭州）为例，其它Region请按实际情况填写。
+        String endpoint = "https://oss-cn-shenzhen.aliyuncs.com";
+        // 阿里云账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM用户进行API访问或日常运维，请登录RAM控制台创建RAM用户。
+        // 填写Bucket名称，例如examplebucket。
+        String bucketName = "outin-4bfcaac9c80e11ecbfcd00163e021072";
+        // 填写Object完整路径，完整路径中不能包含Bucket名称，例如exampledir/exampleobject.txt。
+        String objectName = "1642509481706184.jpg";
+        // 填写本地文件的完整路径，例如D:\\localpath\\examplefile.txt。
+        // 如果未指定本地路径，则默认从示例程序所属项目对应本地路径中上传文件。
+        String filePath= "/Users/tfzhu/fh/1642509481706184.jpg";
+
+        // 创建OSSClient实例。
+        OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
+
+        try {
+            // 创建PutObjectRequest对象。
+            PutObjectRequest putObjectRequest = new PutObjectRequest(bucketName, objectName, new File(filePath));
+            // 如果需要上传时设置存储类型和访问权限，请参考以下示例代码。
+            // ObjectMetadata metadata = new ObjectMetadata();
+            // metadata.setHeader(OSSHeaders.OSS_STORAGE_CLASS, StorageClass.Standard.toString());
+            // metadata.setObjectAcl(CannedAccessControlList.Private);
+            // putObjectRequest.setMetadata(metadata);
+
+            // 上传文件。
+            ossClient.putObject(putObjectRequest);
+        } catch (OSSException oe) {
+            System.out.println("Caught an OSSException, which means your request made it to OSS, "
+                    + "but was rejected with an error response for some reason.");
+            System.out.println("Error Message:" + oe.getErrorMessage());
+            System.out.println("Error Code:" + oe.getErrorCode());
+            System.out.println("Request ID:" + oe.getRequestId());
+            System.out.println("Host ID:" + oe.getHostId());
         } finally {
             if (ossClient != null) {
                 ossClient.shutdown();
